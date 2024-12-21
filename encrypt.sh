622dc5b306be63ec3d6c86ecfcff339d2d4a68cf
Zhehao Hu <hu_zhehao@hotmail.com>
2024-12-21 21:48:30 +0100

    initial pagecrypt

---
+++ encrypt.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Set your desired password
+PASSWORD="781969402daddd6fe75bb6f605403a35ae21a0b2"
+
+# Encrypt all HTML files in the _site directory
+for file in _manuscript/*.html; do
+    pagecrypt "$file" "${file}.tmp" -p "$PASSWORD"
+    mv "${file}.tmp" "$file"
+done
