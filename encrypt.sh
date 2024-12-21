d01c6a6bc939289d207087974e57cdb04660cb5c
Zhehao Hu <hu_zhehao@hotmail.com>
2024-12-21 22:06:26 +0100

    establish encryption

--- encrypt.sh
+++ encrypt.sh
@@ -5,6 +5,6 @@ PASSWORD="781969402daddd6fe75bb6f605403a35ae21a0b2"
 
 # Encrypt all HTML files in the _site directory
 for file in _manuscript/*.html; do
-    pagecrypt "$file" "${file}.tmp" -p "$PASSWORD"
+    pagecrypt "$file" "${file}.tmp" "$PASSWORD"
     mv "${file}.tmp" "$file"
 done
